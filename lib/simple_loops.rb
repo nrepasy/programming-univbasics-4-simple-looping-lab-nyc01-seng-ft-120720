37d2b73afc2501130d3cdcde768d54d544259657
nick repasy <nrepasy@gmail.com>
2020-11-26 05:29:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -30,8 +30,9 @@ end
 array = [5, 4, 3, 2, 1]
 def return_string_array(array)
   count = 0 
+  new_array = []
   while count < array.length do 
-    array[count].to_s
+    new_array.push(array[count].to_s)
     count += 1 
   end 
 end
